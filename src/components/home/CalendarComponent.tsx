61fb86d69b7a28ea1b8fd9f6dfbe37a8f3107ac5
Naveendanj <naveenhettiwaththa@gmail.com>
2025-01-05 01:42:42 +0530

    complete the ui of calendar

--- src/components/home/CalendarComponent.tsx
+++ src/components/home/CalendarComponent.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import { Calendar } from "@/components/ui/calendar"
 import { Separator } from "@/components/ui/separator"
+import { Progress } from "@/components/ui/progress"
 
 
 
 const CalendarFooterComponent = () => {
     return (
-        <div className="flex w-full flex-col my-3">
+        <div className="flex w-full flex-col mt-5">
             <div className="flex justify-between">
                 <div className="flex gap-3">
                     <img className="size-5 my-auto" src="https://leetcode.com/_next/static/images/ttt-a2c700529300981c5d372d69bb9bbdaa.png" />
@@ -17,15 +18,25 @@ const CalendarFooterComponent = () => {
             
             <Separator className="mt-3" />
 
-            <div className="flex flex-col w-full">
-                <label className="text-sm mt-3 text-white font-bold hover:cursor-pointer hover:underline">24-Hour Code Arena</label>
+            <div className="flex mb-4 flex-col w-full">
+                <label className="text-sm mt-5 text-white font-bold ">24-Hour Code Arena</label>
 
                 <div className="mt-3 w-full flex flex-row">
-                    <img className="size-[50px] rounded-md" src="https://assets.leetcode.com/study_plan_v2/dynamic-programming/cover" />
+                    <img className="size-[50px] my-auto rounded-md" src="https://assets.leetcode.com/study_plan_v2/dynamic-programming/cover" />
+                    <div className="flex flex-col my-auto w-full px-2 ml-2 gap-2">
+                        <label className="text-xs hover:cursor-pointer hover:underline">Weekly blitz-coding contest</label>
+                        <div className="w-full flex gap-2">
+                            <Progress className="my-auto"  value={33} />
+                            <label className="text-xs my-auto hover:cursor-pointer hover:underline">28%</label>
+                        </div>
+                        <label className="text-xs mt-[-5px] text-gray-400 hover:cursor-pointer hover:underline">position : 5 / 50000</label>
+
+                    </div>
                 </div>
 
             </div>
 
+            {/* <Separator className="mt-3" /> */}
 
         </div>
     )
